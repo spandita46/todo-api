1b3722af5c685685987c50168a78306db67b1bbd
Sandeep Pandita <sandeeppandita46@gmail.com>
2018-09-19 00:55:55 +0530

    Added GET Endpoint

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {
+    ObjectID
+} = require('mongodb');
 
 const {
     mongoose
@@ -37,6 +40,34 @@ app.get('/todos', (req, res) => {
         res
             .send({
                 todos
+            });
+    }).catch((err) => {
+        res
+            .status(400)
+            .send(err);
+    });
+});
+
+app.get('/todos/:id', (req, res) => {
+    const {
+        id
+    } = req.params;
+
+    if (!ObjectID.isValid(id)) {
+        res
+            .status(404)
+            .send("Invalid Id");
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            res
+                .status(404)
+                .send();
+        }
+        res
+            .send({
+                todo
             });
     }).catch((err) => {
         res
