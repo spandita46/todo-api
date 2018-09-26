2b1e9791722081a7e5429df0333ec6a7ccc5bc4d
Sandeep Pandita <sandeeppandita46@gmail.com>
2018-09-27 00:21:37 +0530

    Update API

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {
@@ -88,6 +89,47 @@ app.delete('/todos/:id', (req, res) => {
     }
 
     Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo) {
+            res
+                .status(404)
+                .send("Not A Valid ToDo");
+        }
+        res
+            .send({
+                todo
+            });
+    }).catch((err) => {
+        res
+            .status(400)
+            .send(err);
+    });
+});
+
+app.patch('/todos/:id', (req, res) => {
+    const {
+        id
+    } = req.params;
+
+    const body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)) {
+        res
+            .status(404)
+            .send("Invalid Id");
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {
+        $set: body
+    }, {
+        new: true
+    }).then((todo) => {
         if (!todo) {
             res
                 .status(404)
